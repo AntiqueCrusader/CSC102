365dc35bc8f402002acd96442a8a244067eb5d31
AntiqueCrusader <darthvador.darth60@gmail.com>
2022-01-26 21:15:20 -0600

    Update Efficency at HQ.js

--- Efficency at HQ.js
+++ Efficency at HQ.js
@@ -6,7 +6,7 @@ function goodloops() {
                 document.getElementById("countdown").innerHTML = "Blastoff!!!";
                 TimeTime = TimeTime - 1;
                 play();
-                Pic()
+                changeImg()
             }, 1000 * i);
         } else if (i > 6) {
             setTimeout(function () {
@@ -29,7 +29,7 @@ function LandLoops() {
                 document.getElementById("countdown").innerHTML = "Landed on the Moon!!!";
                 TimeTime = TimeTime - 1;
                 play2();
-                Pic2()
+                changeImg()
             }, 1000 * i);
         } else if (i > 6) {
             setTimeout(function () {
@@ -48,25 +48,16 @@ function play() {
 	var audio = document.getElementById("audio1");
     audio.play();
 	}
-function Pic(){
-    var image = document.getElementById('Room');
-    if (image.src.match("Room")) {
-    image.src ='Launch.gif'
-}
-}
 function play2() {
 	var audio = document.getElementById("audio2");
     audio.play();
 	}
-    function Pic2(){
+    function changeImg() {
         var image = document.getElementById('Room');
-        if (image.src.match("Room")) {
-        image.src ='EagleLanding.jpg'
+        if (image.src.match("ControlRoom.jpg")) {
+            image.src = "Launch.gif";
+        }
+        else {
+            image.src = "EagleLanding.jpg";
         }
     }
-  //  function loop(){
-       // for(i = 0; 1 < 3; i = i + 1){
-          //  console.log(i)
-           
-         //   if ( i == 3) {break;}
-     //   }
